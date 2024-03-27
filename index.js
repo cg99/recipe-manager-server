6633cbe2d54fca9404ad7216b8882fec377b56d9
cg99 <darkinfinitespace@gmail.com>
2024-03-27 13:49:54 +1100

    minimal server

--- index.js
+++ index.js
@@ -1,44 +1,10 @@
-const express = require('express')
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
-const port = 3000
-
-// Mongo DB configuration
-// const uri = "mongodb+srv://sydney:sydney123@cluster0.i9jjvi0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("RecipeDB").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
-
+const express = require('express');
+const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World! I am recipe Manager!');
-})
+    res.send('<p>Local Explorer Server is running!</p>');
+});
 
-app.get('/recipe', (req, res) => {
-    console.log('recipe requested');
-    res.send('this is a recipe');
-  })
 
-  
-
-app.listen(()=>console.log('server running'));
+// Start the Express server
+app.listen();
